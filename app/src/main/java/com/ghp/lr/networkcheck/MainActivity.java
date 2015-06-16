461e141195c77b9ccc8b2a9de3174908fe711962
huiping.guo <huiping.guo@jimubox.com>
2015-06-16 17:47:39 +0800

    ping+dig

--- app/src/main/java/com/ghp/lr/networkcheck/MainActivity.java
+++ app/src/main/java/com/ghp/lr/networkcheck/MainActivity.java
@@ -2,37 +2,49 @@ package com.ghp.lr.networkcheck;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.ghp.lr.networkcheck.task.DigTask;
+import com.ghp.lr.networkcheck.task.PingTask;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements View.OnClickListener {
+    private Button testbtn;
+    private TextView pingResult;
+    private TextView digResult;
+    private static ExecutorService sExecutor =  Executors.newFixedThreadPool(2);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+   private void initView(){
+       testbtn= (Button) findViewById(R.id.testbtn);
+       testbtn.setOnClickListener(this);
+       pingResult= (TextView) findViewById(R.id.pingResult);
+       digResult= (TextView) findViewById(R.id.digResult);
+   }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.testbtn:
+                pingResult.setText("");
+                digResult.setText("");
+                DigTask digUtils  = new DigTask(digResult);
+                digUtils.executeOnExecutor(sExecutor);
+                PingTask pingUtils  = new PingTask(pingResult);
+                pingUtils.executeOnExecutor(sExecutor);
+                break;
+            default:
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
